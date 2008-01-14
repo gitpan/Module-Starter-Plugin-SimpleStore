6de790c695b66a79c92ed7bda7d6d6ec3db6c388
Ricardo SIGNES <rjbs@cpan.org>
2008-01-14 15:08:05 +0000

    Import of RJBS/Module-Starter-Plugin-SimpleStore-0.143 from CPAN.
    gitpan-cpan-distribution: Module-Starter-Plugin-SimpleStore
    gitpan-cpan-version:      0.143
    gitpan-cpan-path:         RJBS/Module-Starter-Plugin-SimpleStore-0.143.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Module/Starter/Plugin/DirStore.pm
+++ lib/Module/Starter/Plugin/DirStore.pm
@@ -3,8 +3,9 @@ use strict;
 
 package Module::Starter::Plugin::DirStore;
 
-our $VERSION = '0.142';
+our $VERSION = '0.143';
 
+use Carp ();
 use File::Basename;
 
 =head1 NAME
@@ -13,9 +14,7 @@ Module::Starter::Plugin::DirStore -- module template files in a directory
 
 =head1 VERSION
 
-version 0.142
-
- $Id: /my/cs/projects/ms/store/trunk/lib/Module/Starter/Plugin/DirStore.pm 28054 2006-11-14T23:54:25.455657Z rjbs  $
+version 0.143
 
 =head1 SYNOPSIS
 
